4a3eb15828731550ce372a70d3d152877a582503
Мартыненко Артем Сергеевич <artem.martynenko@privatbank.ua>
2021-10-04 17:05:06 +0300

    first commit

--- src/test/java/homework19/DynamicControlTest.java
+++ src/test/java/homework19/DynamicControlTest.java
@@ -1,13 +1,12 @@
 package homework19;
 
-import homework17.DynamicControlPage;
 import org.testng.annotations.Test;
 
 public class DynamicControlTest {
     @Test
     public void dynControlTest(){
 
-        homework17.DynamicControlPage dynamicControlPage = new DynamicControlPage();
+        DynamicControlPage dynamicControlPage = new DynamicControlPage();
         dynamicControlPage.setCheckbox();
         dynamicControlPage.addRemoveCheckboxButton(true);
         dynamicControlPage.addRemoveCheckboxButton(false);

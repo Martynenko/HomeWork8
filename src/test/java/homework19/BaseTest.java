f7ad30ad3cd7ab448ef9cb2783535957b59ae838
Мартыненко Артем Сергеевич <artem.martynenko@privatbank.ua>
2021-10-04 17:45:21 +0300

    first commit

---
+++ src/test/java/homework19/BaseTest.java
@@ -0,0 +1,26 @@
+package homework19;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+public class BaseTest {
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    @BeforeClass
+    public void beforeClass() {
+        WebDriverManager.operadriver().setup();
+        driver = new OperaDriver();
+        wait = new WebDriverWait(driver, 10);
+
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void afterClass() {
+        driver.quit();
+    }
+}

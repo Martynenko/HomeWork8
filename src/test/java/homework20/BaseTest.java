961971504e9e0924a63e9bcc0638b20aae05911d
Мартыненко Артем Сергеевич <artem.martynenko@privatbank.ua>
2021-10-08 12:25:25 +0300

    first commit

--- src/test/java/homework20/BaseTest.java
+++ src/test/java/homework20/BaseTest.java
@@ -1,7 +1,12 @@
 package homework20;
 
+import com.codeborne.selenide.commands.TakeScreenshot;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.Attachment;
+import lombok.SneakyThrows;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -10,8 +15,14 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.opera.OperaDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 public class BaseTest {
     protected WebDriver driver;
@@ -35,5 +46,19 @@ public class BaseTest {
     @AfterClass(alwaysRun = true)
     public void afterClass() {
         driver.quit();
+    }
+
+    @SneakyThrows
+    @AfterMethod
+    public void takeScreenshot(ITestResult result) {
+        if (!result.isSuccess()) {
+            File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            saveScreenshot(Files.readAllBytes(screenshot.toPath()));
+        }
+    }
+
+    @Attachment(value = "Page screenshot", type = "image/png")
+    public byte[] saveScreenshot(byte[] screenShot) {
+        return screenShot;
     }
 }
